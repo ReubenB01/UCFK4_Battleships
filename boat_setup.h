0075600eabee52b9585b2b2cab7ef74a5432c192
Treasa Glubb <tgl24@uclive.ac.nz>
2022-10-17 02:40:24 +0000

    Changed the functions so the new variable could be passed into rotation

--- boat_setup.h
+++ boat_setup.h
@@ -6,6 +6,8 @@
 #include "display.h"
 #include "tinygl.h"
 #include "button.h"
+#include "pacer.h"
+#include "game.h"
 
 typedef struct {
     tinygl_point_t start;
@@ -14,17 +16,16 @@ typedef struct {
 
 int button_pressed_p (void);
 
+Points_t rotate(Points_t points, uint8_t);
+
 Points_t points_init(int8_t start_x, int8_t end_x, int8_t start_y, int8_t end_y);
 
-Points_t boat_movement(Points_t , int* , int*);
+Points_t boat_movement(Points_t , int* , int*, uint8_t);
 
 Points_t boat_size(uint8_t i);
 
 void set_up(int* matrix);
 
 void display_boats(int* matrix);
-
-
-
 
 #endif
