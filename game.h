180d7819e614940895b0d9be2c4121f09d80b602
Reuben Brown <rbr99@uclive.ac.nz>
2022-10-17 05:55:29 +0000

    includes new led_display module

--- game.h
+++ game.h
@@ -11,6 +11,7 @@
 #include "../fonts/font3x5_1.h"
 #include "ir_uart.h"
 #include "gameplay.h"
+#include "led_display.h"
 
 
 #define PACER_RATE 500
@@ -23,30 +24,6 @@
 #define LOSE 'l'
 #define NUM_BOATS 4
 
-/**
-Takes coordinate player has fired at. 
-displays "HIT" message if a ship was placed on chosen coordinate.
-displays "MISS" message if no ship on chosen coordinate.
-coordinate is set to 2 if there was a hit.
-coordinate is set to 3 is there was a miss.
-*/ 
-void hit_message (int board[7][5], int x_cord, int y_cord, int* count);
-
-
-/**
-if matrix coordinate = 2, blink the led.
-if matrix coordinate = 3, leave led on.
-*/
-void display_hits(int board[7][5]);
-
-
-/**
-initialise tinygl.
-set text font size to 3x5.
-set text to scroll.
-set text scroll speed to the message rate.
-*/
-void inititalise_tinygl(void);
-
 
 #endif
+
