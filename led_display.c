e2a7beb77af21892b194fb1ed9512ca1bd43af32
Treasa Glubb <tgl24@uclive.ac.nz>
2022-10-17 22:19:28 +0000

    FInal

--- led_display.c
+++ led_display.c
@@ -7,6 +7,13 @@
 
 #include "led_display.h"
 
+
+/**
+initialise tinygl.
+set text font size to 5x5.
+set text to scroll.
+set text scroll speed to the message rate.
+*/
 void inititalise_tinygl(void)
 {
     tinygl_init(1750);
@@ -17,6 +24,13 @@ void inititalise_tinygl(void)
 }
 
 
+/**
+Takes coordinate player has fired at, the hit count and the shot matrix 
+displays "HIT" message if a ship was placed on chosen coordinate.
+displays "MISS" message if no ship on chosen coordinate.
+coordinate is set to 1 if there was a hit.
+coordinate is set to 2 is there was a miss.
+*/ 
 void hit_message (int matrix[7][5], int x, int y, int* count)
 {
     int (*ptr)[5];
@@ -41,6 +55,12 @@ void hit_message (int matrix[7][5], int x, int y, int* count)
 }
 
 
+/**
+if matrix coordinate = 1, blink the led -- this means that 
+the shot was a hit.
+if matrix coordinate = 2, leave led on -- this means that
+the shot was a miss.
+*/
 void display_hits(int shot_matrix[7][5])
 {
     int x = 0;
@@ -62,6 +82,7 @@ void display_hits(int shot_matrix[7][5])
             }
         }
     }
+
     if (blink_timer == BLINKING) { // this changes the state of the LED when the LED counter is reached
         blink_timer = 0;
         if (led_state ==0) {
@@ -70,20 +91,25 @@ void display_hits(int shot_matrix[7][5])
             led_state = 0;
         }
     }
+
     tinygl_update();
-    blink_timer++; // increnents the blinking counter
+    blink_timer++;
 }
 
+
+/*
+Takes shot count
+If player has sunk all the ships (7 HITS) a win screen
+appears, a losing message sends to other player
+*/
 void win_screen(int* count)
 {
-    if (*count == 7) // if count == 7, all ships are sunk
-    {
+    if (*count == 7) { // if count == 7, all ships are sunk
         tinygl_clear();
         tinygl_text("WIN"); //display win message on winner's board
         ir_uart_putc(LOSE);
         
-        while(1)
-        {
+        while(1) {
             pacer_wait();
             tinygl_update();
             
@@ -91,12 +117,14 @@ void win_screen(int* count)
     }   
 }
 
-
+/*
+If the losing message is recieved it appears on
+the user's screen
+*/
 void lose_screen(void) {
     tinygl_clear();
     tinygl_text("LOSE"); //display win message on winner's board
-        while(1)
-        {
+        while(1) {
             pacer_wait();
             tinygl_update();
         }
